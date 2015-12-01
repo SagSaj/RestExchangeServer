9abd712669cfd6d818219fb15fa7ae5662f67c31
SagSaj <SagSaj@mail.ru>
2015-12-01 13:46:18 +0300

    Update server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res)
 {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	var query = url.parse(req.url, true).query;
-	res.end('Hello ' + (query.name ? query.name : 'stranger'));
+	res.end('Hello!!!');
 
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
